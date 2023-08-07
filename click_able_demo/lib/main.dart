5243867b693c265fb5c7b9c3c0282b97b9444120
Muhammad Abdullah <39946504+abdullahwale@users.noreply.github.com>
2023-08-07 18:40:05 +0500

    Dice App Complete

--- click_able_demo/lib/main.dart
+++ click_able_demo/lib/main.dart
@@ -23,29 +23,108 @@ class DiceApp extends StatefulWidget {
 }
 
 class _DiceAppState extends State<DiceApp> {
-  String name="dice";
-  String ext="jpeg";
-  String url="dice.jpeg";
+  int num = 1;
+  int num1 = 1;
+  int counter = 1;
+
+  void Shuffle1() {
+    if (num == 1) {
+      num = 2;
+      counter = counter + 2;
+    } else if (num == 2) {
+      num = 3;
+      counter = counter + 3;
+    } else if (num == 3) {
+      num = 4;
+      counter = counter + 4;
+    } else if (num == 4) {
+      num = 5;
+      counter = counter + 5;
+    } else if (num == 5) {
+      num = 6;
+      counter = counter + 6;
+    } else {
+      num = 1;
+      counter = counter + 1;
+    }
+    if(counter>=200)
+      counter=1;
+  }
+
+  void Shuffle2() {
+    //int num1=1;
+    if (num1 == 1)
+      num1 = 2;
+    else if (num1 == 2)
+      num1 = 3;
+    else if (num1 == 3)
+      num1 = 4;
+    else if (num1 == 4)
+      num1 = 5;
+    else if (num1 == 5)
+      num1 = 6;
+    else
+      num1 = 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: (){
-            setState(() {
-             url="download.png";
-            });
-            print("Clicked");
-          },
-          child: Image(
-            image: AssetImage('images/$url'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                counter.toString(),
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+              Text(
+                counter.toString(),
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+            ],
           ),
-        ),
+          Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      Shuffle1();
+                    });
+                    print("Clicked");
+                  },
+                  child: Image(
+                    image: AssetImage('images/d$num.png'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      Shuffle2();
+                    });
+                    print("Clicked");
+                  },
+                  child: Image(
+                    image: AssetImage('images/d$num1.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ],
       ),
     );
   }
 }
-
-
-
-
---
+++ xylo_with_pakage/android/app/src/main/kotlin/com/example/xylo_with_pakage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xylo_with_pakage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
